be034e8ef7113c62337dd70d0f0bc348e440e2ce
Gaantz <ca.palomino.rivera@gmail.com>
2017-05-28 08:57:02 -0500

    Initial Commit

---
+++ RxHearthstone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxHearthstone
+//
+//  Created by Cristian Palomino Rivera on 28/05/17.
+//  Copyright © 2017 Cristian Palomino Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
