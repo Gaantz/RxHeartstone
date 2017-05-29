fc00618a82524c1d36734bb46facbe014eaf0a76
Gaantz <ca.palomino.rivera@gmail.com>
2017-05-29 08:12:27 -0500

    primero pasos

---
+++ RxHearthstone/Info.swift
@@ -0,0 +1,9 @@
+//
+//  Info.swift
+//  RxHearthstone
+//
+//  Created by Cristian Palomino Rivera on 28/05/17.
+//  Copyright © 2017 Cristian Palomino Rivera. All rights reserved.
+//
+
+import Foundation
